f7da8cfd34ff8c194c9119173e1898fcb35727f2
tonisj98 <antonio.serjime@gmail.com>
2022-03-09 13:25:50 +0100

    f

--- js/app.js
+++ js/app.js
@@ -1,2 +1,47 @@
 // Tu código aquí.
 
+let botonRestar = document.querySelector("#restar")
+botonRestar.onclick = restar
+
+let botonSumar = document.querySelector("#sumar")
+botonSumar.onclick = sumar
+
+let contador
+contador = document.querySelector("#counter").textContent;
+let estilo
+
+
+function restar(contador){
+
+    console.log("resto")
+    contador = document.querySelector("#counter").textContent;
+    
+    contador = contador - 1
+ 
+    document.querySelector("#counter").textContent = contador;
+    estilo = document.querySelector("#counter");
+
+    
+    if (contador < 0) {
+        estilo.style.color = "red"
+    }
+    
+
+}
+function sumar(contador){
+
+    console.log("sumo")
+    contador = document.querySelector("#counter").textContent;
+    contador = Math.floor(contador)
+    console.log(typeof(contador))
+
+    contador = contador + 1
+ 
+    document.querySelector("#counter").textContent = contador;
+    estilo = document.querySelector("#counter");
+    
+    if (contador > 0) {
+        estilo.style.color = "blue"
+    }
+    
+    }
